727d9ef2247c27c8efa2c38527b3f741aa125703
Zied Harzi <zharzi@student.42angouleme.fr>
2023-01-27 17:46:47 +0100

    27jan23 fix check syntax

--- srcs/parsing/ft_check_syntax.c
+++ srcs/parsing/ft_check_syntax.c
@@ -6,7 +6,7 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 11:58:19 by zharzi            #+#    #+#             */
-/*   Updated: 2023/01/20 12:57:26 by zharzi           ###   ########.fr       */
+/*   Updated: 2023/01/27 17:46:04 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@ int	ft_check_syntax(char **src, char **trans)
 	if (!ft_check_format_quotes(trans[0], ft_strlen(trans[0])) \
 		|| !ft_check_format_angl_brackets(src[0], trans[0]) \
 			|| !ft_check_format_pipes(trans[0]))
+	{
+		write(2, "minishell : syntax error\n", 25);
 		return (0);
+	}
 	return (1);
 }
